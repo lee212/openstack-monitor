33c848f8347c7e937abf963f15bf55f35bd1ea52
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2015-05-19 14:27:07 -0400

    tests for openstack

---
+++ novatest.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import tests
+import time
+import os
+from os.path import expanduser, abspath
+import uuid
+
+
+def fullpath(path):
+    return abspath(expanduser(path))
+
+
+def getopts(args=None):
+    from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+    p = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+
+    default_name_format = '{user}-{pid}-{uuid}'
+
+    p.add_argument('-n', '--name', default='<'+default_name_format+'>',
+                   help='Name of the instance'.
+                   format(default_name_format))
+    p.add_argument('-k', '--key-name', required=True,
+                   help='The key name to use')
+    p.add_argument('-f', '--ssh-key-path', default='~/.ssh/id_rsa',
+                   help='Path to the private ssh key')
+    p.add_argument('-t', '--times', default='times.txt',
+                   help='Save execution times here')
+
+    opts = p.parse_args(args=args)
+
+    if opts.name.startswith('<') and opts.name.endswith('>'):
+        opts.name = default_name_format.format(
+            user=os.getenv('LOGNAME'),
+            pid=os.getpid(),
+            uuid=uuid.uuid1().hex)
+
+    opts.ssh_key_path = fullpath(opts.ssh_key_path)
+
+    return opts
+
+
+def run_tests(name=None, key=None, ssh_key=None):
+    assert name is not None
+    assert key is not None
+    assert ssh_key is not None
+
+    tester = tests.NovaTester()
+    tester.boot(name=name, key_name=key)
+
+    try:
+        tester.get_ip()
+        tester.ping()
+        tester.wait_for_sshd()
+        tester.ssh(key=ssh_key)
+    finally:
+        tester.cleanup()
+
+
+def main(opts):
+    today = int(time.time())
+    t0 = time.time()
+    run_tests(name=opts.name, key=opts.key_name, ssh_key=opts.ssh_key_path)
+    t = time.time() - t0
+
+    with open(opts.times, 'a') as fd:
+        fd.write('{today},{time}\n'
+                 .format(today=today, time=t))
+
+
+if __name__ == '__main__':
+    opts = getopts()
+    print opts
+    main(opts)
---
+++ tests.py
@@ -0,0 +1,167 @@
+from pyshc.sh import Sh
+from time import sleep
+import subprocess
+import pipes
+
+
+nova = Sh('nova')
+ping = Sh('ping')
+ssh = Sh('ssh')
+nc = Sh('nc')
+
+
+class TestFailure(Exception):
+    pass
+
+
+def get_machine_property(nova_show_output, property):
+    def get_value(line, key, sep='|'):
+        split = line.split(sep)
+
+        if not len(split) == 4:
+            return None
+
+        key_found = split[1].strip()
+        if key_found == key:
+            return split[2].strip()
+
+        else:
+            return None
+
+    if type(nova_show_output) is str:
+        nova_show_output = nova_show_output.split('\n')
+
+    for line in nova_show_output:
+        val = get_value(line, property)
+        if val:
+            return val
+
+
+def get_machine_id(output):
+    return get_machine_property(output, 'id')
+
+
+def get_machine_intnet(output):
+    return get_machine_property(output, 'int-net network')
+
+
+class NovaTester(object):
+    def __init__(self, attempts=10, delay=10):
+        self._machine = None  # the machine id
+        self._int_ip = None  # the internal ipv4
+
+        self.attempts = attempts  # no. attempts to retreive instance info
+        self.delay = delay        # delay between attempts in seconds
+
+    def cleanup(self):
+        "Deletes the instance"
+        print('Deleting {}'.format(self._machine))
+        try:
+            nova(['delete', self._machine])
+        except subprocess.CalledProcessError, e:
+            raise TestFailure(
+                'Could not delete instance {machine} exit {ret}\n{out}'
+                .format(machine=self._machine, ret=e.returncode, out=e.output))
+
+    def boot(self, name=None, key_name=None,
+             image='futuresystems/ubuntu-14.04', flavor='m1.small'):
+
+        assert self._machine is None
+        assert name is not None
+        assert key_name is not None
+
+        boot = ['boot',
+                '--flavor', flavor,
+                '--image', image,
+                '--key_name', key_name,
+                name
+                ]
+
+        print('Executing {}'.format(' '.join(map(pipes.quote, boot))))
+
+        try:
+            out = nova(boot)
+        except subprocess.CalledProcessError, e:
+            raise TestFailure(
+                ' '.join([
+                    'Boot command {cmd} failed with non-zero status {ret}\n'
+                    .format(cmd=e.cmd, ret=e.returncode),
+                    '\nSTDERR\n########################################\n',
+                    e.output
+                    ]))
+
+        machine = get_machine_id(out)
+        self._machine = machine
+        print('Machine {}'.format(machine))
+
+    def get_ip(self):
+        assert self._machine is not None
+
+        print('Getting IPv4 for {}'.format(self._machine))
+        for i in xrange(self.attempts):
+            output = nova(['show', self._machine])
+            ip = get_machine_intnet(output)
+            if ip:
+                self._int_ip = ip
+                return ip
+            sleep(self.delay)
+        raise TestFailure(
+            'Exceeded max attempts {max} to retrieve IP address for {machine}'
+            .format(max=self.attempts, machine=self._machine))
+
+    def ping(self, count=10):
+        if not self._int_ip:
+            self.get_ip()
+        print('Ping {machine} at {addr}'.format(machine=self._machine,
+                                                addr=self._int_ip))
+        try:
+            ping(['-c', str(count), self._int_ip])
+        except subprocess.CalledProcessError, e:
+            raise TestFailure(
+                ('Command {cmd} failed {ret}'
+                 + ' when pinging machine {machine} at {addr}'
+                 + '\n'
+                 + 'ERROR########################################\n'
+                 + '{err}\n')
+                .format(cmd=e.cmd,
+                        ret=e.returncode,
+                        machine=self._machine,
+                        addr=self._int_ip,
+                        err=e.output))
+
+    def wait_for_sshd(self, attempts=None):
+        assert self._int_ip is not None
+        attempts = attempts or self.attempts
+
+        print('Waiting for sshd to start on {machine} at {addr}'
+              .format(machine=self._machine, addr=self._int_ip))
+
+        for i in xrange(attempts):
+            try:
+                nc(['-z', '-v', self._int_ip, '22'])
+            except subprocess.CalledProcessError:
+                sleep(self.delay)
+                continue
+            return True
+
+        raise TestFailure('Timeout waiting for sshd to start')
+
+    def ssh(self, user='ubuntu', key='~/.ssh/id_rsa', remote_cmd='ifconfig'):
+        assert self._int_ip is not None
+        ssh_args = ['-o', 'StrictHostKeyChecking=no',
+                    '-o', 'UserKnownHostsFile=/dev/null'
+                    ]
+        cmd = ssh_args + ['{user}@{addr}'.format(user=user, addr=self._int_ip),
+                          remote_cmd]
+        try:
+            out = ssh(cmd)
+        except subprocess.CalledProcessError, e:
+            raise TestFailure(
+                ('Failed to ssh to machine {machine} at {addr} with {ret}\n',
+                 + 'STDOUT\n########################################\n',
+                 + out + '\n',
+                 + 'STDERR\n########################################\n',
+                 + e.output)
+                .format(machine=self._machine,
+                        addr=self._int_ip,
+                        ret=e.returncode))
